78811bd5567c61724dfa09ce8cb49fb4df0bae03
MohamedGamal3594 <mohamedgamal3172@gmail.com>
2023-09-02 06:34:24 -0700

    Add .gitignore file

---
+++ Carty/view/ProductsTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductsTableCell.swift
+//  Carty
+//
+//  Created by Jimmy on 02/09/2023.
+//
+
+import UIKit
+
+class ProductsTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
